0d90ad7befdef9a69a43f34373ea40ec996aa885
Kiv Aujla <89060170+kaujla1@users.noreply.github.com>
2023-06-10 15:24:17 -0400

    Create basic grid setup

---
+++ script.js
@@ -0,0 +1,47 @@
+//create a webpage with a 16x16 grid of square divs
+
+//given a number (x)
+//create x*x divs 
+//format divs so there are only x divs per row 
+//then the rest wrap onto the next line
+//thus creating a x by x grid 
+
+const gridContainer = document.querySelector(".grid-container")
+
+let numOfDivs = 16;
+
+let row;
+
+function createRows(numRows) {
+  for (i = 0; i < numRows; i++) {
+    row = document.createElement("div");
+    row.classList.add("row", "row" + i);
+    row.style.height = "20px";
+    gridContainer.appendChild(row);
+  }
+}
+
+let newDiv;
+
+/*
+function createDivs(numDivs) {
+  for (i = 0; i < numDivs; i++) {
+    newDiv = document.createElement("div");
+    row = document.querySelector(".row" + i);
+    row.appendChild(newDiv);
+  }
+}*/
+
+function createDivs(divs) {
+  for (g = 0; g < divs; g++) {
+    for (i = 0; i < divs; i++) {
+      newDiv = document.createElement("div");
+      row = document.querySelector(".row" + i);
+      row.appendChild(newDiv);
+    }
+  }
+}
+
+createRows(numOfDivs);
+createDivs(numOfDivs);
+
